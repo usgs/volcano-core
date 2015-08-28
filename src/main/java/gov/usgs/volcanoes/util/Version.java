662e8adedac14d83411cec3484268e807c8aa8e3
Tom Parker <tparker@usgs.gov>
2015-08-28 14:38:11 -0800

    Add Version to .gitignore

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.util;
 
 public final class Version {
 
-    public static final String BUILD_TIME="2015-08-28T21:21:03Z";
+    public static final String BUILD_TIME="2015-08-28T22:20:12Z";
 
     public static final String POM_VERSION="1.1.4";
 
