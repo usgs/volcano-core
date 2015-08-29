da8b928138c046c35093aab75ecba77443fd35d7
Tom Parker <tparker@usgs.gov>
2015-08-28 16:07:25 -0800

    Add coveralls report to pom

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.util;
 
 public final class Version {
 
-    public static final String BUILD_TIME="2015-08-28T22:20:12Z";
+    public static final String BUILD_TIME="2015-08-29T00:01:50Z";
 
     public static final String POM_VERSION="1.1.4";
 
