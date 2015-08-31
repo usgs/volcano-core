7df5518440a4a47c1ebc7f73913799616ac408ef
Tom Parker <tparker@usgs.gov>
2015-08-31 08:40:09 -0800

    Continue ConfigFile refactor

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,9 +2,9 @@ package gov.usgs.volcanoes.util;
 
 public final class Version {
 
-    public static final String BUILD_TIME="2015-08-29T23:41:17Z";
+    public static final String BUILD_TIME="2015-08-31T01:49:14Z";
 
-    public static final String POM_VERSION="1.1.6";
+    public static final String POM_VERSION="1.1.7-SNAPSHOT";
 
 	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
 }
--- src/test/java/gov/usgs/volcanoes/util/configFile/ConfigFileTest.java
+++ src/test/java/gov/usgs/volcanoes/util/configFile/ConfigFileTest.java
@@ -1,8 +1,17 @@
 package gov.usgs.volcanoes.util.configFile;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ConfigFileTest {
@@ -11,13 +20,43 @@ public class ConfigFileTest {
 	
 	private ConfigFile configFile;
 	
+	@BeforeClass
+	public static void setUpClass() throws IOException {
+		InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream(CONFIG_FILENAME);
+		Path defaultPath = new File(CONFIG_FILENAME).toPath();
+		Files.copy(is, defaultPath, StandardCopyOption.REPLACE_EXISTING);
+	}
+	
 	@Before
-	public void setUp() {
+	public void setUp() throws FileNotFoundException {
+		configFile = new ConfigFile(CONFIG_FILENAME);
 	}
 
 	@Test(expected = FileNotFoundException.class)
-	public void test() throws FileNotFoundException {
+	public void when_configDoesnNotExist_then_throwHelpfulException() throws FileNotFoundException {
 		configFile = new ConfigFile("does not exist");
+	}
+	
+	@Test
+	public void when_askedForDouble_then_returnDouble() {
+		double d = configFile.getDouble("double");
+		assertEquals(d, 3.14);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void when_askedForDouble_then_returnError() {
+		configFile.getDouble("string");
+	}
+
+	@Test
+	public void when_askedForInt_then_returnInt() {
+		int i = configFile.getInt("int");
+		assertEquals(i, 5);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void when_askedForInt_then_returnError() {
+		configFile.getDouble("string");
 	}
 
 }
