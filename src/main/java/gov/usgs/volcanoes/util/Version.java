8fd5b7ca741625046b82d81fd98bf4614deb04b5
Tom Parker <tparker@usgs.gov>
2015-08-31 12:56:35 -0800

    Remove types package. Rework when needed.

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.util;
 
 public final class Version {
 
-    public static final String BUILD_TIME="2015-08-31T01:49:14Z";
+    public static final String BUILD_TIME="2015-08-31T17:48:01Z";
 
     public static final String POM_VERSION="1.1.7-SNAPSHOT";
 
