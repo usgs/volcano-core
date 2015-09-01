5094d8f8b01dcf31f99ef8e07b028056baf3c122
Tom Parker <tparker@usgs.gov>
2015-08-31 18:22:42 -0800

    Tweak args package-info

--- src/main/java/gov/usgs/volcanoes/util/args/package-info.java
+++ src/main/java/gov/usgs/volcanoes/util/args/package-info.java
@@ -1,7 +1,7 @@
 /**
  * A wrapper to JSAP, providing a simple and consistent way to handle java
  * command line arguments.
- * 
+ * <P>
  * Create an Args object and wrap it with decorators as needed. A decorator is
  * not required to be compatible with all other decorators. Some combinations
  * just don't make sense.
