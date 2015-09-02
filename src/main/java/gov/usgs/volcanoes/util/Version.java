fde35fc5fb0884b7c4397ba0a1034bd2ece7aabe
Tom Parker <tparker@usgs.gov>
2015-09-02 15:33:10 -0800

    Add issues stanza to pom

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.util;
 
 public interface Version {
 
-    public static final String BUILD_TIME="2015-09-02T22:02:49Z";
+    public static final String BUILD_TIME="2015-09-02T23:20:58Z";
 
     public static final String POM_VERSION="1.1.7-SNAPSHOT";
 
--- src/test/java/gov/usgs/volcanoes/util/args/ArgsFacadeTest.java
+++ src/test/java/gov/usgs/volcanoes/util/args/ArgsFacadeTest.java
@@ -19,5 +19,4 @@ public class ArgsFacadeTest {
 		ArgsFacade.main("-k");
 		assertTrue(true);
 	}
-
 }
