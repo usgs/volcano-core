670c50bfdf5ffdc33a5b8c44faae5e13b22fbaa4
Tom Parker <tparker@usgs.gov>
2015-09-02 16:20:41 -0800

    Continue work on maven site

--- src/main/java/gov/usgs/volcanoes/util/Version.java
+++ src/main/java/gov/usgs/volcanoes/util/Version.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.util;
 
 public interface Version {
 
-    public static final String BUILD_TIME="2015-09-02T23:20:58Z";
+    public static final String BUILD_TIME="2015-09-03T00:13:40Z";
 
     public static final String POM_VERSION="1.1.7-SNAPSHOT";
 
