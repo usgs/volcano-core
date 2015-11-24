5fe894ae6a59ed7d316287720e7cff7ea423a941
Tom Parker <tparker@usgs.gov>
2015-11-23 21:29:05 -0900

    Add package-level docs.

---
+++ src/main/java/gov/usgs/volcanoes/core/contrib/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * contrib holds classes contributed by others. Each file has is covered by its own license as indicated in the file.
+ *
+ */
+package gov.usgs.volcanoes.core.contrib;
---
+++ src/main/java/gov/usgs/volcanoes/core/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Mostly, but not exclusively, utility classes.
+ *
+ */
+package gov.usgs.volcanoes.core.util;
