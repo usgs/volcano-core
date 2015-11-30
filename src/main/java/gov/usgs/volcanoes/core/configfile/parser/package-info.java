b8089fe4744fdd91e99b6e133e32b55afc6d15e8
Tom Parker <tparker@usgs.gov>
2015-11-30 09:58:04 -0900

    Add javadocs

---
+++ src/main/java/gov/usgs/volcanoes/core/configfile/parser/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Parsers to extract values from parameters provided in a config file.
+ * 
+ */
+package gov.usgs.volcanoes.core.configfile.parser;
---
+++ src/main/java/gov/usgs/volcanoes/core/math/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes to perform mathematical functions.
+ * 
+ */
+package gov.usgs.volcanoes.core.math;
