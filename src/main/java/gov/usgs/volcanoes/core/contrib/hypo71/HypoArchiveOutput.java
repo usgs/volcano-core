bec8cf1c52c5c2df6098acf6a23519a75a5e9aa9
Diana Norgaard <dnorgaard@usgs.gov>
2017-07-12 15:01:31 -0700

    format

--- src/main/java/gov/usgs/volcanoes/core/contrib/hypo71/HypoArchiveOutput.java
+++ src/main/java/gov/usgs/volcanoes/core/contrib/hypo71/HypoArchiveOutput.java
@@ -15,40 +15,44 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class HypoArchiveOutput {
 
-	private LinkedList<Station> stations = new LinkedList<Station>();
-	private ControlCard controlCard;
-	private LinkedList<PhaseRecord> phaseRecords = new LinkedList<PhaseRecord>();
-	private LinkedList<CrustalModel> crustalModel = new LinkedList<CrustalModel>();
-	
-	@XmlElements(value = { @XmlElement })
-	public LinkedList<Station> getStations() {
-		return stations;
-	}
-	public void setStations(LinkedList<Station> stations) {
-		this.stations = stations;
-	}
-	
-	@XmlElement
-	public ControlCard getControlCard() {
-		return controlCard;
-	}
-	public void setControlCard(ControlCard controlCard) {
-		this.controlCard = controlCard;
-	}
-	
-	@XmlElements(value = { @XmlElement })
-	public LinkedList<PhaseRecord> getPhaseRecords() {
-		return phaseRecords;
-	}
-	public void setPhaseRecords(LinkedList<PhaseRecord> phasRecords) {
-		this.phaseRecords = phasRecords;
-	}
-	
-	@XmlElements(value = { @XmlElement })
-	public LinkedList<CrustalModel> getCrustalModel() {
-		return crustalModel;
-	}
-	public void setCrustalModel(LinkedList<CrustalModel> crustalModel) {
-		this.crustalModel = crustalModel;
-	}
+  private LinkedList<Station> stations = new LinkedList<Station>();
+  private ControlCard controlCard;
+  private LinkedList<PhaseRecord> phaseRecords = new LinkedList<PhaseRecord>();
+  private LinkedList<CrustalModel> crustalModel = new LinkedList<CrustalModel>();
+
+  @XmlElements(value = {@XmlElement})
+  public LinkedList<Station> getStations() {
+    return stations;
+  }
+
+  public void setStations(LinkedList<Station> stations) {
+    this.stations = stations;
+  }
+
+  @XmlElement
+  public ControlCard getControlCard() {
+    return controlCard;
+  }
+
+  public void setControlCard(ControlCard controlCard) {
+    this.controlCard = controlCard;
+  }
+
+  @XmlElements(value = {@XmlElement})
+  public LinkedList<PhaseRecord> getPhaseRecords() {
+    return phaseRecords;
+  }
+
+  public void setPhaseRecords(LinkedList<PhaseRecord> phasRecords) {
+    this.phaseRecords = phasRecords;
+  }
+
+  @XmlElements(value = {@XmlElement})
+  public LinkedList<CrustalModel> getCrustalModel() {
+    return crustalModel;
+  }
+
+  public void setCrustalModel(LinkedList<CrustalModel> crustalModel) {
+    this.crustalModel = crustalModel;
+  }
 }
