462ffff32b03631d576383022eb74b595a028fc2
Tom Parker <tparker@usgs.gov>
2017-11-17 08:37:52 -0900

    cleanup

--- src/main/java/gov/usgs/volcanoes/core/ui/GridBagHelper.java
+++ src/main/java/gov/usgs/volcanoes/core/ui/GridBagHelper.java
@@ -20,102 +20,93 @@ import java.util.StringTokenizer;
  * <p>iy=... ipady value</p>
  * <p>i=...,...,...,...  top inset, left inset, bottom inset, right inset</p>
  * <p>f=b|h|v|n fill both, horisontal, vertical, none</p>
- * <p>a=n|e|s|w|ne|nw|se|sw|c anchor north, east, south, west, northeast, northwest, southeast, southwest, center</p>
+ * <p>a=n|e|s|w|ne|nw|se|sw|c 
+ * anchor north, east, south, west, northeast, northwest, southeast, southwest, center</p>
  * 
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2005/04/15 04:44:38  cervelli
- * Comments.
- *
  * @author Dan Cervelli
  */
-public class GridBagHelper
-{
-	/**
-	 * Default constructor 
-	 */
-	private GridBagHelper() {}
-	
-	/**
-	 * @param c GridBagConstraints to process
-	 * @param cmd command of 'key=value' type
-	 */
-	private static void parseCommand(GridBagConstraints c, String cmd)
-	{
-		String key = cmd.substring(0, cmd.indexOf("="));
-		String value = cmd.substring(cmd.indexOf("=") + 1);
-		if (key.equals("x"))
-			c.gridx = Integer.parseInt(value);
-		else if (key.equals("y"))
-			c.gridy = Integer.parseInt(value);
-		else if (key.equals("w"))
-			c.gridwidth = Integer.parseInt(value);
-		else if (key.equals("h"))
-			c.gridheight = Integer.parseInt(value);
-		else if (key.equals("wx"))
-			c.weightx = Double.parseDouble(value);
-		else if (key.equals("wy"))
-			c.weighty = Double.parseDouble(value);
-		else if (key.equals("ix"))
-			c.ipadx = Integer.parseInt(value);
-		else if (key.equals("iy"))
-			c.ipady = Integer.parseInt(value);
-		else if (key.equals("i"))
-		{
-			StringTokenizer st2 = new StringTokenizer(value, ",");
-			int i1 = Integer.parseInt(st2.nextToken());
-			int i2 = Integer.parseInt(st2.nextToken());
-			int i3 = Integer.parseInt(st2.nextToken());
-			int i4 = Integer.parseInt(st2.nextToken());
-			c.insets = new Insets(i1, i2, i3, i4);
-		}
-		else if (key.equals("f"))
-		{
-			if (value.equals("b"))
-				c.fill = GridBagConstraints.BOTH;
-			else if (value.equals("h"))
-				c.fill = GridBagConstraints.HORIZONTAL;
-			else if (value.equals("v"))
-				c.fill = GridBagConstraints.VERTICAL;
-			else if (value.equals("n"))
-				c.fill = GridBagConstraints.NONE;
-		}
-		else if (key.equals("a"))
-		{
-			if (value.equals("n"))
-				c.anchor = GridBagConstraints.NORTH;
-			else if (value.equals("e"))
-				c.anchor = GridBagConstraints.EAST;
-			else if (value.equals("s"))
-				c.anchor = GridBagConstraints.SOUTH;
-			else if (value.equals("w"))
-				c.anchor = GridBagConstraints.WEST;
-			else if (value.equals("ne"))
-				c.anchor = GridBagConstraints.NORTHEAST;
-			else if (value.equals("nw"))
-				c.anchor = GridBagConstraints.NORTHWEST;
-			else if (value.equals("se"))
-				c.anchor = GridBagConstraints.SOUTHEAST;
-			else if (value.equals("sw"))
-				c.anchor = GridBagConstraints.SOUTHWEST;
-			else if (value.equals("c"))
-				c.anchor = GridBagConstraints.CENTER;
-		}
-	}
+public class GridBagHelper {
+  /**
+   * Default constructor .
+   */
+  private GridBagHelper() {}
 
-	/**
-	 * Processing method
-	 * @param c GridBagConstraints to process
-	 * @param s command string to parse
-	 * @return initialized GridBagConstraints
-	 */
-	public static GridBagConstraints set(GridBagConstraints c, String s)
-	{
-		StringTokenizer st = new StringTokenizer(s, ";");
-		while (st.hasMoreTokens())
-		{
-			String cmd = st.nextToken();
-			parseCommand(c, cmd);
-		}
-		return c;
-	}
+  /**
+   * Parse.
+   * @param constraints GridBagConstraints to process
+   * @param cmd command of 'key=value' type
+   */
+  private static void parseCommand(GridBagConstraints constraints, String cmd) {
+    String key = cmd.substring(0, cmd.indexOf("="));
+    String value = cmd.substring(cmd.indexOf("=") + 1);
+    if (key.equals("x")) {
+      constraints.gridx = Integer.parseInt(value);
+    } else if (key.equals("y")) {
+      constraints.gridy = Integer.parseInt(value);
+    } else if (key.equals("w")) {
+      constraints.gridwidth = Integer.parseInt(value);
+    } else if (key.equals("h")) {
+      constraints.gridheight = Integer.parseInt(value);
+    } else if (key.equals("wx")) {
+      constraints.weightx = Double.parseDouble(value);
+    } else if (key.equals("wy")) {
+      constraints.weighty = Double.parseDouble(value);
+    } else if (key.equals("ix")) {
+      constraints.ipadx = Integer.parseInt(value);
+    } else if (key.equals("iy")) {
+      constraints.ipady = Integer.parseInt(value);
+    } else if (key.equals("i")) {
+      StringTokenizer st2 = new StringTokenizer(value, ",");
+      int i1 = Integer.parseInt(st2.nextToken());
+      int i2 = Integer.parseInt(st2.nextToken());
+      int i3 = Integer.parseInt(st2.nextToken());
+      int i4 = Integer.parseInt(st2.nextToken());
+      constraints.insets = new Insets(i1, i2, i3, i4);
+    } else if (key.equals("f")) {
+      if (value.equals("b")) {
+        constraints.fill = GridBagConstraints.BOTH;
+      } else if (value.equals("h")) {
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+      } else if (value.equals("v")) {
+        constraints.fill = GridBagConstraints.VERTICAL;
+      } else if (value.equals("n")) {
+        constraints.fill = GridBagConstraints.NONE;
+      } else if (key.equals("a")) {
+        if (value.equals("n")) {
+          constraints.anchor = GridBagConstraints.NORTH;
+        } else if (value.equals("e")) {
+          constraints.anchor = GridBagConstraints.EAST;
+        }
+      } else if (value.equals("s")) {
+        constraints.anchor = GridBagConstraints.SOUTH;
+      } else if (value.equals("w")) {
+        constraints.anchor = GridBagConstraints.WEST;
+      } else if (value.equals("ne")) {
+        constraints.anchor = GridBagConstraints.NORTHEAST;
+      } else if (value.equals("nw")) {
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+      } else if (value.equals("se")) {
+        constraints.anchor = GridBagConstraints.SOUTHEAST;
+      } else if (value.equals("sw")) {
+        constraints.anchor = GridBagConstraints.SOUTHWEST;
+      } else if (value.equals("c")) {
+        constraints.anchor = GridBagConstraints.CENTER;
+      }
+    }
+  }
+
+  /**
+   * Processing method.
+   * @param constraints GridBagConstraints to process
+   * @param cmdStr command string to parse
+   * @return initialized GridBagConstraints
+   */
+  public static GridBagConstraints set(GridBagConstraints constraints, String cmdStr) {
+    StringTokenizer st = new StringTokenizer(cmdStr, ";");
+    while (st.hasMoreTokens()) {
+      String cmd = st.nextToken();
+      parseCommand(constraints, cmd);
+    }
+    return constraints;
+  }
 }
--- src/main/java/gov/usgs/volcanoes/core/xml/SimpleXmlParser.java
+++ src/main/java/gov/usgs/volcanoes/core/xml/SimpleXmlParser.java
@@ -9,39 +9,52 @@ import java.util.Stack;
  * Quick and Dirty xml parser. This parser is, like the SAX parser, an event
  * based parser, but with much less functionality.
  * 
- * Reformatted and Java 1.5-ized from article:
+ * <p>Reformatted and Java 1.5-ized from article:
  * http://www.javaworld.com/javatips/jw-javatip128_p.html
  * 
  * @author Steven R. Brandt, Dan Cervelli
  */
-public class SimpleXMLParser {
+public class SimpleXmlParser {
   /**
-   * Return popped value from st if non-empty, else PRE
+   * Return popped value from st if non-empty, else PRE.
    * @param st Stack
    * @return popped value or, if none, PRE
    */
   private static int popMode(Stack<Integer> st) {
-    if (!st.empty())
+    if (!st.empty()) {
       return ((Integer) st.pop()).intValue();
-    else
+    } else {
       return PRE;
+    }
   }
 
-  private final static int TEXT = 1, ENTITY = 2, OPEN_TAG = 3, CLOSE_TAG = 4, START_TAG = 5,
-      ATTRIBUTE_LVALUE = 6, ATTRIBUTE_EQUAL = 9, ATTRIBUTE_RVALUE = 10, QUOTE = 7, IN_TAG = 8,
-      SINGLE_TAG = 12, COMMENT = 13, DONE = 11, DOCTYPE = 14, PRE = 15, CDATA = 16;
+  private static final int TEXT = 1;
+  private static final int ENTITY = 2;
+  private static final int OPEN_TAG = 3;
+  private static final int CLOSE_TAG = 4;
+  private static final int START_TAG = 5;
+  private static final int ATTRIBUTE_LVALUE = 6;
+  private static final int ATTRIBUTE_EQUAL = 9;
+  private static final int ATTRIBUTE_RVALUE = 10;
+  private static final int QUOTE = 7;
+  private static final int IN_TAG = 8;
+  private static final int SINGLE_TAG = 12;
+  private static final int COMMENT = 13;
+  private static final int DONE = 11;
+  private static final int DOCTYPE = 14;
+  private static final int PRE = 15;
+  private static final int CDATA = 16;
 
   /**
    * Parse character stream into xml document.
    * @param doc event handler realizing actions to perform while event happens
-   * @param r character-stream reader with processed xml text
-   * @throws Exception
+   * @param reader character-stream reader with processed xml text
+   * @throws Exception when things go wrong
    */
-  public static void parse(XMLDocHandler doc, Reader r) throws Exception {
+  public static void parse(XmlDocHandler doc, Reader reader) throws Exception {
     Stack<Integer> st = new Stack<Integer>();
     int depth = 0;
     int mode = PRE;
-    int c = 0;
     int quotec = '"';
     depth = 0;
     StringBuffer sb = new StringBuffer();
@@ -52,23 +65,25 @@ public class SimpleXMLParser {
     Map<String, String> attrs = null;
     st = new Stack<Integer>();
     doc.startDocument();
-    int line = 1, col = 0;
+    int line = 1;
+    int col = 0;
     boolean eol = false;
-    while ((c = r.read()) != -1) {
+    int ch = 0;
+    while ((ch = reader.read()) != -1) {
 
       // We need to map \r, \r\n, and \n to \n
       // See XML spec section 2.11
-      if (c == '\n' && eol) {
+      if (ch == '\n' && eol) {
         eol = false;
         continue;
       } else if (eol) {
         eol = false;
-      } else if (c == '\n') {
+      } else if (ch == '\n') {
         line++;
         col = 0;
-      } else if (c == '\r') {
+      } else if (ch == '\r') {
         eol = true;
-        c = '\n';
+        ch = '\n';
         line++;
         col = 0;
       } else {
@@ -81,56 +96,57 @@ public class SimpleXMLParser {
 
         // We are between tags collecting text.
       } else if (mode == TEXT) {
-        if (c == '<') {
+        if (ch == '<') {
           st.push(mode);
           mode = START_TAG;
           if (sb.length() > 0) {
             doc.text(sb.toString());
             sb.setLength(0);
           }
-        } else if (c == '&') {
+        } else if (ch == '&') {
           st.push(mode);
           mode = ENTITY;
           etag.setLength(0);
-        } else
-          sb.append((char) c);
-
+        } else {
+          sb.append((char) ch);
+        }
         // we are processing a closing tag: e.g. </foo>
       } else if (mode == CLOSE_TAG) {
-        if (c == '>') {
+        if (ch == '>') {
           mode = popMode(st);
           tagName = sb.toString();
           sb.setLength(0);
           depth--;
-          if (depth == 0)
+          if (depth == 0) {
             mode = DONE;
+          }
           doc.endElement(tagName);
         } else {
-          sb.append((char) c);
+          sb.append((char) ch);
         }
 
         // we are processing CDATA
       } else if (mode == CDATA) {
-        if (c == '>' && sb.toString().endsWith("]]")) {
+        if (ch == '>' && sb.toString().endsWith("]]")) {
           sb.setLength(sb.length() - 2);
           doc.text(sb.toString());
           sb.setLength(0);
           mode = popMode(st);
-        } else
-          sb.append((char) c);
-
+        } else {
+          sb.append((char) ch);
+        }
         // we are processing a comment. We are inside
         // the <!-- .... --> looking for the -->.
       } else if (mode == COMMENT) {
-        if (c == '>' && sb.toString().endsWith("--")) {
+        if (ch == '>' && sb.toString().endsWith("--")) {
           sb.setLength(0);
           mode = popMode(st);
-        } else
-          sb.append((char) c);
-
+        } else {
+          sb.append((char) ch);
+        }
         // We are outside the root tag element
       } else if (mode == PRE) {
-        if (c == '<') {
+        if (ch == '<') {
           mode = TEXT;
           st.push(mode);
           mode = START_TAG;
@@ -139,10 +155,11 @@ public class SimpleXMLParser {
         // We are inside one of these <? ... ?>
         // or one of these <!DOCTYPE ... >
       } else if (mode == DOCTYPE) {
-        if (c == '>') {
+        if (ch == '>') {
           mode = popMode(st);
-          if (mode == TEXT)
+          if (mode == TEXT) {
             mode = PRE;
+          }
         }
 
         // we have just seen a < and
@@ -150,54 +167,57 @@ public class SimpleXMLParser {
         // <foo>, </foo>, <!-- ... --->, etc.
       } else if (mode == START_TAG) {
         mode = popMode(st);
-        if (c == '/') {
+        if (ch == '/') {
           st.push(mode);
           mode = CLOSE_TAG;
-        } else if (c == '?') {
+        } else if (ch == '?') {
           mode = DOCTYPE;
         } else {
           st.push(mode);
           mode = OPEN_TAG;
           tagName = null;
           attrs = new HashMap<String, String>();
-          sb.append((char) c);
+          sb.append((char) ch);
         }
 
         // we are processing an entity, e.g. &lt;, &#187;, etc.
       } else if (mode == ENTITY) {
-        if (c == ';') {
+        if (ch == ';') {
           mode = popMode(st);
           String cent = etag.toString();
           etag.setLength(0);
-          if (cent.equals("lt"))
+          if (cent.equals("lt")) {
             sb.append('<');
-          else if (cent.equals("gt"))
+          } else if (cent.equals("gt")) {
             sb.append('>');
-          else if (cent.equals("amp"))
+          } else if (cent.equals("amp")) {
             sb.append('&');
-          else if (cent.equals("quot"))
+          } else if (cent.equals("quot")) {
             sb.append('"');
-          else if (cent.equals("apos"))
+          } else if (cent.equals("apos")) {
             sb.append('\'');
-          else if (cent.startsWith("#x"))
+          } else if (cent.startsWith("#x")) {
             sb.append((char) Integer.parseInt(cent.substring(2), 16));
-          else if (cent.startsWith("#"))
+          } else if (cent.startsWith("#")) {
             sb.append((char) Integer.parseInt(cent.substring(1)));
-          // Insert custom entity definitions here
-          else
+            // Insert custom entity definitions here
+          } else {
             exc("Unknown entity: &" + cent + ";", line, col);
+          }
         } else {
-          etag.append((char) c);
+          etag.append((char) ch);
         }
 
         // we have just seen something like this:
         // <foo a="b"/
         // and are looking for the final >.
       } else if (mode == SINGLE_TAG) {
-        if (tagName == null)
+        if (tagName == null) {
           tagName = sb.toString();
-        if (c != '>')
+        }
+        if (ch != '>') {
           exc("Expected > for tag: <" + tagName + "/>", line, col);
+        }
         doc.startElement(tagName, attrs);
         doc.endElement(tagName);
         if (depth == 0) {
@@ -213,116 +233,118 @@ public class SimpleXMLParser {
         // like this <foo ... >. It could
         // still be a <!-- ... --> or something.
       } else if (mode == OPEN_TAG) {
-        if (c == '>') {
-          if (tagName == null)
+        if (ch == '>') {
+          if (tagName == null) {
             tagName = sb.toString();
+          }
           sb.setLength(0);
           depth++;
           doc.startElement(tagName, attrs);
           tagName = null;
           attrs = new HashMap<String, String>();
           mode = popMode(st);
-        } else if (c == '/') {
+        } else if (ch == '/') {
           mode = SINGLE_TAG;
-        } else if (c == '-' && sb.toString().equals("!-")) {
+        } else if (ch == '-' && sb.toString().equals("!-")) {
           mode = COMMENT;
-        } else if (c == '[' && sb.toString().equals("![CDATA")) {
+        } else if (ch == '[' && sb.toString().equals("![CDATA")) {
           mode = CDATA;
           sb.setLength(0);
-        } else if (c == 'E' && sb.toString().equals("!DOCTYP")) {
+        } else if (ch == 'E' && sb.toString().equals("!DOCTYP")) {
           sb.setLength(0);
           mode = DOCTYPE;
-        } else if (Character.isWhitespace((char) c)) {
+        } else if (Character.isWhitespace((char) ch)) {
           tagName = sb.toString();
           sb.setLength(0);
           mode = IN_TAG;
         } else {
-          sb.append((char) c);
+          sb.append((char) ch);
         }
 
         // We are processing the quoted right-hand side
         // of an element's attribute.
       } else if (mode == QUOTE) {
-        if (c == quotec) {
+        if (ch == quotec) {
           rvalue = sb.toString();
           sb.setLength(0);
           attrs.put(lvalue, rvalue);
           mode = IN_TAG;
           // See section the XML spec, section 3.3.3
           // on normalization processing.
-        } else if (" \r\n\u0009".indexOf(c) >= 0) {
+        } else if (" \r\n\t".indexOf(ch) >= 0) {
           sb.append(' ');
-        } else if (c == '&') {
+        } else if (ch == '&') {
           st.push(mode);
           mode = ENTITY;
           etag.setLength(0);
         } else {
-          sb.append((char) c);
+          sb.append((char) ch);
         }
 
       } else if (mode == ATTRIBUTE_RVALUE) {
-        if (c == '"' || c == '\'') {
-          quotec = c;
+        if (ch == '"' || ch == '\'') {
+          quotec = ch;
           mode = QUOTE;
-        } else if (Character.isWhitespace((char) c)) {
+        } else if (Character.isWhitespace((char) ch)) {
           ;
         } else {
           exc("Error in attribute processing", line, col);
         }
 
       } else if (mode == ATTRIBUTE_LVALUE) {
-        if (Character.isWhitespace((char) c)) {
+        if (Character.isWhitespace((char) ch)) {
           lvalue = sb.toString();
           sb.setLength(0);
           mode = ATTRIBUTE_EQUAL;
-        } else if (c == '=') {
+        } else if (ch == '=') {
           lvalue = sb.toString();
           sb.setLength(0);
           mode = ATTRIBUTE_RVALUE;
         } else {
-          sb.append((char) c);
+          sb.append((char) ch);
         }
 
       } else if (mode == ATTRIBUTE_EQUAL) {
-        if (c == '=') {
+        if (ch == '=') {
           mode = ATTRIBUTE_RVALUE;
-        } else if (Character.isWhitespace((char) c)) {
+        } else if (Character.isWhitespace((char) ch)) {
           ;
         } else {
           exc("Error in attribute processing.", line, col);
         }
 
       } else if (mode == IN_TAG) {
-        if (c == '>') {
+        if (ch == '>') {
           mode = popMode(st);
           doc.startElement(tagName, attrs);
           depth++;
           tagName = null;
           attrs = new HashMap<String, String>();
-        } else if (c == '/') {
+        } else if (ch == '/') {
           mode = SINGLE_TAG;
-        } else if (Character.isWhitespace((char) c)) {
+        } else if (Character.isWhitespace((char) ch)) {
           ;
         } else {
           mode = ATTRIBUTE_LVALUE;
-          sb.append((char) c);
+          sb.append((char) ch);
         }
       }
     }
-    if (mode == DONE)
+    if (mode == DONE) {
       doc.endDocument();
-    else
+    } else {
       exc("missing end tag", line, col);
+    }
   }
 
   /**
-   * Generates error diagnostic message and throws exception with it
-   * @param s text error description
+   * Generates error diagnostic message and throws exception with it.
+   * @param str text error description
    * @param line line number
    * @param col column number
-   * @throws Exception
+   * @throws Exception when things go wrong
    */
-  private static void exc(String s, int line, int col) throws Exception {
-    throw new Exception(s + " near line " + line + ", column " + col);
+  private static void exc(String str, int line, int col) throws Exception {
+    throw new Exception(str + " near line " + line + ", column " + col);
   }
 }
